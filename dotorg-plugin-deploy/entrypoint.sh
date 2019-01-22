4f324a226cefd45c0790d6cd24a9017bfbbf9147
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-01-22 10:19:19 -0600

    Add reassuring messages and fix an errant curly quote

--- dotorg-plugin-deploy/entrypoint.sh
+++ dotorg-plugin-deploy/entrypoint.sh
@@ -21,11 +21,13 @@ SVN_DIR="${GITHUB_WORKSPACE}/svn-${SLUG}"
 
 # Checkout just trunk and assets for efficiency
 # Tagging will be handled on the SVN level
-echo "\Checking out .org repository...\n"
+echo "\nChecking out .org repository...\n"
 svn checkout --depth immediates $SVN_URL $SVN_DIR
 cd $SVN_DIR
 svn update --set-depth infinity assets
 svn update --set-depth infinity trunk
+
+echo "\nCopying files...\n"
 
 # Copy from current branch to /trunk, excluding dotorg assets
 rsync -r --exclude "$GITHUB_WORKSPACE/$ASSETS_DIR" "$GITHUB_WORKSPACE/*" trunk/
@@ -35,8 +37,12 @@ rsync -r "$GITHUB_WORKSPACE/$ASSETS_DIR/*" assets/
 
 # Add everything and commit to SVN
 # The force flag ensures we recurse into subdirectories even if they are already added
+echo "\nCommitting files...\n"
 svn add * --force
 svn commit -m "Update to version $VERSION from GitHub"
 
 # SVN tag to VERSION
-svn cp "^/trunk" "^/tags/$VERSION" -m "Tag $VERSION”
+echo "\nTagging version...\n"
+svn cp "^/trunk" "^/tags/$VERSION" -m "Tag $VERSION"
+
+echo "\nPlugin deployed!\n"
