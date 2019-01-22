80f86f0eba3b152bbcf18238c309384d1fdaa98a
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-01-22 08:21:11 -0600

    Better method of SVN checkout using sparse directories

--- dotorg-plugin-deploy/entrypoint.sh
+++ dotorg-plugin-deploy/entrypoint.sh
@@ -15,15 +15,17 @@ fi
 SVN_URL="http://plugins.svn.wordpress.org/${SLUG}/"
 SVN_DIR="${GITHUB_WORKSPACE}/svn-${SLUG}"
 
-# Checkout just trunk and assets for efficiency
-# Tagging will be handled on the SVN level
-echo "\Checking out .org repository...\n"
-svn checkout "$SVN_URL/trunk" "$SVN_DIR/trunk"
-svn checkout "$SVN_URL/assets" "$SVN_DIR/assets"
-
 # Checkout stable branch of repo - can you do this inside GITHUB_WORKSPACE?
 # Do I need to go back to master afterward since more actions can run?
 # Does this need a customizable ENV?
+
+# Checkout just trunk and assets for efficiency
+# Tagging will be handled on the SVN level
+echo "\Checking out .org repository...\n"
+svn checkout --depth immediates $SVN_URL $SVN_DIR
+cd $SVN_DIR
+svn update --set-depth infinity assets
+svn update --set-depth infinity trunk
 
 # Copy from stable branch to /trunk, excluding dotorg assets
 
