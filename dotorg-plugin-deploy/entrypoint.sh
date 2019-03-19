6a73a8f440d0ae87addf19354036612a55986f1c
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-03-19 09:24:54 -0600

    Add a clearer comment about secrets

--- dotorg-plugin-deploy/entrypoint.sh
+++ dotorg-plugin-deploy/entrypoint.sh
@@ -7,6 +7,9 @@
 set -eo
 
 # Ensure SVN username and password are set
+# IMPORTANT: while secrets are encrypted and not viewable in the GitHub UI,
+# they are by necessity provided as plaintext in the context of the Action,
+# so do not echo or use debug mode unless you want your secrets exposed!
 if [[ -z "$SVN_USERNAME" ]]; then
 	echo "Set the SVN_USERNAME secret"
 	exit 1
