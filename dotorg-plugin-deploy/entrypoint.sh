9e162d167cf9d99605eaf256b2065c8bbb32857c
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-03-28 15:55:38 -0600

    Also set Git name and email in Bash

--- dotorg-plugin-deploy/entrypoint.sh
+++ dotorg-plugin-deploy/entrypoint.sh
@@ -6,6 +6,8 @@
 # it does not exit with a 0, and I only care about the final exit.
 set -eo
 
+git config --global user.email "10upbot+github@10up.com" && git config --global user.name "10upbot on GitHub"
+
 # Ensure SVN username and password are set
 # IMPORTANT: while secrets are encrypted and not viewable in the GitHub UI,
 # they are by necessity provided as plaintext in the context of the Action,
