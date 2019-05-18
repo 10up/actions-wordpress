214819853fd7dda06b89e5f3048aa6c413dae42e
Helen Hou-Sandi <helen.y.hou@gmail.com>
2019-05-18 00:53:08 -0400

    New Action: Deploy assets/readme updates (#11)

---
+++ dotorg-plugin-asset-update/README.md
@@ -0,0 +1,47 @@
+# WordPress.org Plugin Assets Update
+
+This Action commits any `readme.txt` and WordPress.org-specific `assets` changes in your specified stable branch (typically `master`) to the WordPress.org plugin repository if no other changes have been made. This is useful for updating things like screenshots or `Tested up to` separately from functional changes provided your Git branching methodology avoids changing anything else in the specified branch between functional releases.
+
+## Configuration
+
+### Required secrets
+* `SVN_USERNAME`
+* `SVN_PASSWORD`
+
+Secrets can be set while editing your workflow or in the repository settings. They cannot be viewed once stored. [GitHub secrets documentation](https://developer.github.com/actions/creating-workflows/storing-secrets/)
+
+### Optional environment variables
+* `SLUG` - defaults to the respository name, customizable in case your WordPress repository has a different slug. This should be a very rare case as WordPress assumes that the directory and initial plugin file have the same slug.
+* `ASSETS_DIR` - defaults to `.wordpress-org`, customizable for other locations of WordPress.org plugin repository-specific assets that belong in the top-level `assets` directory (the one on the same level as `trunk`)
+
+## Example Workflow File
+```
+workflow "Plugin Asset Update" {
+  resolves = ["WordPress Plugin Asset Update"]
+  on = "push"
+}
+
+# Filter for master branch
+action "branch" {
+    uses = "actions/bin/filter@master"
+    args = "branch master"
+}
+
+action "WordPress Plugin Asset Update" {
+  needs = ["branch"]
+  uses = "10up/actions-wordpress/dotorg-plugin-asset-update@master"
+  secrets = ["SVN_USERNAME", "SVN_PASSWORD"]
+}
+```
+
+## Contributing
+Want to help? Check out our [contributing guidelines](../CONTRIBUTING.md) to get started.
+
+<p align="center">
+<a href="http://10up.com/contact/"><img src="https://10updotcom-wpengine.s3.amazonaws.com/uploads/2016/10/10up-Github-Banner.png" width="850"></a>
+</p>
+
+## License
+
+Our GitHub Actions are available for use and remix under the MIT license.
+
---
+++ dotorg-plugin-asset-update/entrypoint.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Note that this does not use pipefail because if the grep later
+# doesn't match I want to be able to show an error first
+set -eo
+
+# Ensure SVN username and password are set
+# IMPORTANT: while secrets are encrypted and not viewable in the GitHub UI,
+# they are by necessity provided as plaintext in the context of the Action,
+# so do not echo or use debug mode unless you want your secrets exposed!
+if [[ -z "$SVN_USERNAME" ]]; then
+	echo "Set the SVN_USERNAME secret"
+	exit 1
+fi
+
+if [[ -z "$SVN_PASSWORD" ]]; then
+	echo "Set the SVN_PASSWORD secret"
+	exit 1
+fi
+
+# Allow some ENV variables to be customized
+if [[ -z "$SLUG" ]]; then
+	SLUG=${GITHUB_REPOSITORY#*/}
+fi
+echo "ℹ︎ SLUG is $SLUG"
+
+if [[ -z "$ASSETS_DIR" ]]; then
+	ASSETS_DIR=".wordpress-org"
+fi
+echo "ℹ︎ ASSETS_DIR is $ASSETS_DIR"
+
+SVN_URL="http://plugins.svn.wordpress.org/${SLUG}/"
+SVN_DIR="/github/svn-${SLUG}"
+
+# Checkout just trunk and assets for efficiency
+# Tagging will be handled on the SVN level
+echo "➤ Checking out .org repository..."
+svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
+cd "$SVN_DIR"
+svn update --set-depth infinity assets
+svn update --set-depth infinity trunk
+
+echo "➤ Copying files..."
+cd "$GITHUB_WORKSPACE"
+
+# "Export" a cleaned copy to a temp directory
+TMP_DIR="/github/archivetmp"
+mkdir "$TMP_DIR"
+
+git config --global user.email "10upbot+github@10up.com"
+git config --global user.name "10upbot on GitHub"
+
+# If there's no .gitattributes file, write a default one into place
+if [[ ! -e "$GITHUB_WORKSPACE/.gitattributes" ]]; then
+	cat > "$GITHUB_WORKSPACE/.gitattributes" <<-EOL
+	/$ASSETS_DIR export-ignore
+	/.gitattributes export-ignore
+	/.gitignore export-ignore
+	/.github export-ignore
+	EOL
+
+	# Ensure we are in the $GITHUB_WORKSPACE directory, just in case
+	# The .gitattributes file has to be committed to be used
+	# Just don't push it to the origin repo :)
+	git add .gitattributes && git commit -m "Add .gitattributes file"
+fi
+
+# This will exclude everything in the .gitattributes file with the export-ignore flag
+git archive HEAD | tar x --directory="$TMP_DIR"
+
+cd "$SVN_DIR"
+
+# Copy from clean copy to /trunk, excluding dotorg assets
+# The --delete flag will delete anything in destination that no longer exists in source
+rsync -r "$TMP_DIR/" trunk/ --delete
+
+# Copy dotorg assets to /assets
+rsync -r "$GITHUB_WORKSPACE/$ASSETS_DIR/" assets/ --delete
+
+# Add everything and commit to SVN
+# The force flag ensures we recurse into subdirectories even if they are already added
+# Suppress stdout in favor of svn status later for readability
+echo "➤ Preparing files..."
+svn add . --force > /dev/null
+
+# SVN delete all deleted files
+# Also suppress stdout here
+svn status | grep '^\!' | sed 's/! *//' | xargs -I% svn rm % > /dev/null
+
+if [[ -z $(svn stat) ]]; then
+	echo "🛑 Nothing to deploy!"
+	exit 78
+# Check if there is more than just the readme.txt modified in trunk
+# The leading whitespace in the pattern is important
+# so it doesn't match potential readme.txt in subdirectories!
+elif svn stat trunk | grep -qvi ' trunk/readme.txt$'; then
+	echo "🛑 Other files have been modified; changes not deployed"
+	exit 1
+fi
+
+echo "➤ Committing files..."
+svn commit -m "Updating readme/assets from GitHub" --no-auth-cache --non-interactive  --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+
+echo "✓ Plugin deployed!"
